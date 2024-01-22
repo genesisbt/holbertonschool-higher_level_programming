e6b20d1b43d9ecc85275adc9240dab8f4f23adcf
german <gerchureloco@gmail.com>
2024-01-22 10:19:08 -0800

    hi

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combi5es remarkable power with very clear syntax"
-print(str.split(' ')[5] + " " +str.split(' ')[6] + " with Python")
+str = str.split(' ')[5] + " " +str.split(' ')[6] + " with Python"
+print(str)
