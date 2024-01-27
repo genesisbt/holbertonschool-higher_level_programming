f335ca032aea081df341c769949a6be549185584
german <gerchureloco@gmail.com>
2024-01-27 09:32:19 -0800

    fix

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-for d1 in range(0, 10):
-    for d2 in range(d1, 10):
-        if d1 != d2:
-            print("{}{}".format(d1, d2), end=', '
-                if d1 != 8 or d2 != 9 else '\n')
+for d in range(0, 10):
+    for e in range(d, 10):
+        if d != e:
+            print("{}{}".format(d, e), end=', ' if d != 8 or e != 9 else '\n')
