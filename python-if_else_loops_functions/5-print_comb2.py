0113d5ba3c33ee1177b992de30344476cbe634b2
german <gerchureloco@gmail.com>
2024-01-27 08:15:12 -0800

    Excercise 0

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -3,5 +3,5 @@ for number in range(0, 99):
     if number < 10:
         print("0{}".format(number), end =', ')
     else:
-        print ("{}".format(number), end =', ' if number == 100 '\n')
+        print ("{}".format(number), end =', ' if number != 98 else '\n')
 
