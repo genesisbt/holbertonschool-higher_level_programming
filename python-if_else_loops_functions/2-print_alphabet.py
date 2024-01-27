6bbcc4763c5d85fad3b9bdfe656705f9490787a8
german <gerchureloco@gmail.com>
2024-01-27 07:15:01 -0800

    Excercise 0

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-for code in range(ord('a'), ord('z') +1):
-    print(f"{chr(code)}", end = '')
+for code in range(ord('a'), ord('z') + 1):
+    print("{}".format(chr(code)), end="")
