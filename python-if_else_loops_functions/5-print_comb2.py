ea3d1c3d6a37d628bcd264d6cc2c2d05bcb5ff62
german <gerchureloco@gmail.com>
2024-01-27 08:00:44 -0800

    Excercise 0

---
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for number in range(0, 99):
+    if number < 10:
+        print("0{}".format(number), end =', ')
+    else:
+        print ("{}".format(number), end =', ' if number == 100 '\n')
+
