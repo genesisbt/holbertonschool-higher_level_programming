d3ba519fa85bd708f50aa7c6376688655d7045ea
german <gerchureloco@gmail.com>
2024-01-27 07:14:36 -0800

    Excercise 0

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for code in range(ord('a'), ord('z') +1):
+for code in range(ord('a'), ord('z') + 1):
     if chr(code) != 'q' and chr(code) != 'e':
         print("{}".format(chr(code)), end="")
