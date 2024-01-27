bf437b6d2e42348ebf7393292b0d47af35acb861
german <gerchureloco@gmail.com>
2024-01-27 09:22:20 -0800

    fix

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -2,4 +2,4 @@
 for digit1 in range(0, 10):
     for digit2 in range(digit1, 10):
         if digit1 != digit2:
-            print("{}{}".format(digit1, digit2), end=', ' if digit1 != 9 or digit2 != 9 else '\n')
+            print("{}{}".format(digit1, digit2), end=', ' if digit1 != 8 or digit2 != 9 else '\n')
