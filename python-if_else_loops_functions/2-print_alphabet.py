004f96646f8fd92bea1b8453f36b87d2473cbed4
german <gerchureloco@gmail.com>
2024-01-27 07:04:19 -0800

    Excercise 0

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 for code in range(ord('a'), ord('z') +1):
     if chr(code) != 'q' and chr(code) != 'e':
-        print(f"{chr(code)}")
+        print(f"{chr(code)}", end = '')
+print("")
