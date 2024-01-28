78d0ebb72b60d86b6c6fb50f9245b81a04ad37b6
german <gerchureloco@gmail.com>
2024-01-27 18:27:39 -0800

    hola

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,5 +1,10 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    new_list = []
+    for i in range(len(my_list)):
+        if my_list[i] % 2 == 0:
+            new_list.append(True)
+        else:
+            new_list.append(False)
+    return new_list
 
-def print_list_integer(my_list=[]):
-    for num in my_list:
-        print("{:d}".format(num))
