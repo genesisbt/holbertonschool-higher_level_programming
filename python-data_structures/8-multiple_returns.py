f2a483cc8adbb1b86da0c219c29e03305bbfe3ab
german <gerchureloco@gmail.com>
2024-01-27 18:08:10 -0800

    hola

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,5 +1,3 @@
 #!/usr/bin/env python3
-
-def print_list_integer(my_list=[]):
-    for num in my_list:
-        print("{:d}".format(num))
+def multiple_returns(sentence):
+    return (len(sentence), sentence[0] if sentence else None)
