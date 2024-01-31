7dc0520a3b9281421a3c3c81ff2c99b84f231188
german <gerchureloco@gmail.com>
2024-01-31 06:13:43 -0800

    ejercicio 0

---
+++ python-more_data_structures/1-search_replace.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    newlist = my_list.copy()
+    for index in range(len(newlist)):
+        if newlist[index] == search:
+           newlist[index] = replace
+    return newlist
