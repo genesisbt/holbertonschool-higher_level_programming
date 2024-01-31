dbdbcfd7e8597155e5a5207970cf8e4c84d094c0
german <gerchureloco@gmail.com>
2024-01-31 05:21:44 -0800

    ejercicio 0

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 def square_matrix_simple(matrix=[]):
-    result = result = list(map(list, map(map(square), matrix)))
-    
+    newmatrix = matrix
+    result = result = list(map(list, map(map(square), newmatrix))) 
     def square(number):
         number = number * number
     return number
