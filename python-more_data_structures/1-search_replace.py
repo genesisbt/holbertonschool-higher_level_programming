619f9ecc47a99ccc7ffe94cd4fc7818679eb4eba
german <gerchureloco@gmail.com>
2024-01-31 06:15:31 -0800

    ejercicio 0

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -3,5 +3,5 @@ def search_replace(my_list, search, replace):
     newlist = my_list.copy()
     for index in range(len(newlist)):
         if newlist[index] == search:
-           newlist[index] = replace
+            newlist[index] = replace
     return newlist
