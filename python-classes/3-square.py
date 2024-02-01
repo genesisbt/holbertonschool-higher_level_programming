532d275f0c8b485792dfe3cd47861ecca8faffca
german <gerchureloco@gmail.com>
2024-02-01 13:51:15 -0800

    second square

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -11,6 +11,15 @@ class Square:
 
     def __init__(self, size=0):
         self.__size = size
+
+        @property
+        def size(self):
+            return(self.__size)
+
+        @size.setter
+        def size(self, number):
+            self.__size = value
+
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
