7362bab280eeb3f77fb57ce87e1cefe3a548dd28
german <gerchureloco@gmail.com>
2024-02-01 13:39:32 -0800

    second square

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -7,9 +7,6 @@ This module defines Square as a class.
 class Square:
     """
     This is a class where a square is defined.
-
-    Attributes:
-    size: is a private instance attribute.
     """
 
     def __init__(self, size=0):
