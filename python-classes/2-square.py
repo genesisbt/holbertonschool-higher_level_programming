0a3bbd589eab2b377761706d787552f3d68da3ed
german <gerchureloco@gmail.com>
2024-02-01 13:30:12 -0800

    second square

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -7,12 +7,12 @@ class Square:
     Class name
 
     Atributes:
-        size: private atribute
+        size (int): private atribute
     """
 
     def __init__(self, size=0):
         self.__size = size
-        if not isinstance(size, int):
+        if type(size) is not int:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >=0")
