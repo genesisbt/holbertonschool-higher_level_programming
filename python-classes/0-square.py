6776a4f77258dc5805f57d41cc9fbde8079bbf09
german <gerchureloco@gmail.com>
2024-02-01 08:33:09 -0800

    list division

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-'''This is a class that defines a square'''
+'''Class that defines a square'''
 
 class Square:
-     '''Initialize an instance of the Square class.'''
+    '''Class definition of the Square class.'''
     pass
