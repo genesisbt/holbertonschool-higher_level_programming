eaedb7b3a413c4f032b85a566edd2af24185f55e
german <gerchureloco@gmail.com>
2024-01-31 18:34:22 -0800

    Excercise 9

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     newint = 0
-    if roman_string == None or type(roman_string) != str:
+    if roman_string is None or type(roman_string) is not str:
         return 0
     for index in range(len(roman_string)):
         newint = add(roman_string[index], newint)
-        if index < len(roman_string) -1:
+        if index < len(roman_string) - 1:
             if roman_string[index] == "I" and roman_string[index + 1] != "I":
                 newint = newint - 2
     return newint
+
 
 def add(char, n1):
     n2 = 0
@@ -26,4 +27,4 @@ def add(char, n1):
         n2 = n1 + 5
     if char == "I":
         n2 = n1 + 1
-    return n2;
+    return n2
