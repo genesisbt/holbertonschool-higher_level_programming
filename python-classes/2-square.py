dc0b527a4c2d8cca16d3d336c9617afbab69b592
german <gerchureloco@gmail.com>
2024-02-01 09:34:42 -0800

    list division

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -18,9 +18,8 @@ class Square:
         TypeError: If the provided size is not an integer.
         ValueError: If the provided size is less than 0.
         """
+        self.__size = size
         if type(size) is not int:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >=0")
-        else:
-            self.__size = size
