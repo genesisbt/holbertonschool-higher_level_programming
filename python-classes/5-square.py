bc6a9dd0f0440b949d7e005ed7c58f4526a44194
german <gerchureloco@gmail.com>
2024-02-01 15:03:41 -0800

    5

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -9,12 +9,17 @@ class Square:
     This is a class where a square is defined.
     """
 
-    def __init__(self, size=0):
+    def __init__(self, size=0, position=0):
         self.__size = size
+        self.__position = position
 
     @property
     def size(self):
         return (self.__size)
+
+    @property
+    def position(self):
+        return (self.__position)
 
     @size.setter
     def size(self, number):
@@ -23,6 +28,15 @@ class Square:
             raise TypeError("size must be an integer")
         elif number < 0:
             raise ValueError("size must be >= 0")
+        
+    @position.setter
+    def position(self, postuple):
+        if not isinstance(postuple, tuple) or not isinstance(postuple[0], int) or not isinstance(postuple[1], int):
+            raise TypeError("position must be an integer")
+        elif postuple[0] < 0 or postuple[1] < 0:
+            raise ValueError("position must be positive")
+        else:
+            self.__position = (number2, number3)
 
     def area(self):
         return self.__size ** 2
