6d95ec318544067ff5114e40eeb5a2cf90dbe68b
german <gerchureloco@gmail.com>
2024-01-31 18:58:53 -0800

    Excercise 9

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    for index in range(x):
+        if index < len(my_list):
+            print("{}".format(my_list[index], end=""))
+    if x < len(my_list):
+        return x
+    return len(my_list)
+    
