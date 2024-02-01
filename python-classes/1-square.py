e47fc06bcd5b2bf2cc396f28c19dde936fddaefb
german <gerchureloco@gmail.com>
2024-02-01 08:44:55 -0800

    list division

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -6,9 +6,9 @@ class Square:
     """
     Class for defining a Square and its atributes
     Attributes:
-    __size (int): Private attribute 
+    __size (int): Private attribute
     """
-    def __init__(self,size):
+    def __init__(self, size):
         """
         Initializes a new Instance of the Class Square
 
