7805276339b8e0e921a40a759cef81636a8e72f1
german <gerchureloco@gmail.com>
2024-01-31 20:00:45 -0800

    Excercise 9

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    try:
-        for index in range(x):
-            print("{}".format(my_list[index]), end="")
-    except IndexError:
-       print()
-       return index
-
+    for index in range(x):
+        try:
+            print(my_list[index], end="")
+        except IndexError:
+            print()
+            return index
     print()
     return index + 1
