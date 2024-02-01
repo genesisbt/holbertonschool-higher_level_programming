8f501a887f2cf30ae93e6460f26897790c822376
german <gerchureloco@gmail.com>
2024-02-01 14:42:46 -0800

    5

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -28,6 +28,8 @@ class Square:
         return self.__size ** 2
 
     def my_print(self):
+        if self.__size == 0:
+            print()
         for unit in range(self.__size):
             for unit2 in range(self.__size):
                 print("#", end="")
