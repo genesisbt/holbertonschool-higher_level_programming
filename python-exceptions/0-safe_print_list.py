8a8271fe6347b36a77eef9e6102facc085f6cb3f
german <gerchureloco@gmail.com>
2024-01-31 20:04:46 -0800

    Excercise 9

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -6,7 +6,7 @@ def safe_print_list(my_list=[], x=0):
         try:
             print(my_list[index], end="")
         except IndexError:
-            print()
-            return index
+            index = index - 1
+            break
     print()
-    return index + 1
+    return index + 1 
