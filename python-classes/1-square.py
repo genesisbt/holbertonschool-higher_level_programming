f612fa61776e5ba3f8ee3a32dab46403daa0d612
german <gerchureloco@gmail.com>
2024-02-01 08:43:38 -0800

    list division

---
+++ python-classes/1-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+'''Class that defines a square'''
+
+
+class Square:
+    """
+    Class for defining a Square and its atributes
+    Attributes:
+    __size (int): Private attribute 
+    """
+    def __init__(self,size):
+        """
+        Initializes a new Instance of the Class Square
+
+        Args:
+            size (int): The size of the square
+        """
+        self.__size = size
