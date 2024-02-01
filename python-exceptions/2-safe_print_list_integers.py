ea28b6fecdea81ffc0b59b97b6454ba4111af469
german <gerchureloco@gmail.com>
2024-01-31 20:53:41 -0800

    Excercise 9

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -5,7 +5,7 @@ def safe_print_list_integers(my_list=[], x=0):
         try:
             print("{:d}".format(my_list[index]), end="")
             nprint += 1
-        except (IndexError, TypeError, ValueError):
+        except (TypeError, ValueError):
             continue
     print()
     return nprint
