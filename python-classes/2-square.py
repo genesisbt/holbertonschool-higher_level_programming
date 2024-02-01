7a55e722687ab41f7fac9d0795945fa0d25b350d
german <gerchureloco@gmail.com>
2024-02-01 09:37:12 -0800

    list division

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -3,21 +3,10 @@
 
 
 class Square:
-    """
-    Class for defining a Square and its atributes
-    Attributes:
-    __size (int): Private attribute
-    """
-    def __init__(self, size):
-        """
-        Initializes a new Instance of the Class Square
+    """ Class for defining a Square and its atributes """
 
-        Args:
-            size (int): The size of the square
-        Raises:
-        TypeError: If the provided size is not an integer.
-        ValueError: If the provided size is less than 0.
-        """
+    def __init__(self, size):
+        """ Initializes a new Instance of the Class Square """
         self.__size = size
         if type(size) is not int:
             raise TypeError("size must be an integer")
