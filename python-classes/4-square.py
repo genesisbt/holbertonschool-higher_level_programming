f62402f86835b0ac088f4dc13d49a6b57cb173cc
german <gerchureloco@gmail.com>
2024-02-01 13:51:44 -0800

    second square

---
+++ python-classes/4-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+This module defines Square as a class.
+"""
+
+
+class Square:
+    """
+    This is a class where a square is defined.
+    """
+
+    def __init__(self, size=0):
+        self.__size = size
+
+        @property
+        def size(self):
+            return(self.__size)
+
+        @size.setter
+        def size(self, number):
+            self.__size = value
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        return self.__size ** 2
