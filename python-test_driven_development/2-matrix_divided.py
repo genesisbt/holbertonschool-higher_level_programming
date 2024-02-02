b6e40f975e5631364a2e2de839b9b689f9762296
german <gerchureloco@gmail.com>
2024-02-02 11:41:33 -0800

    doctest 0

---
+++ python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Module for dividing a matrix.
+"""
+
+
+def matrix_divided(matrix, div):
+    """
+    Receives a matrix and a divisor.
+
+    Args:
+        matrix (list of lists of integers or floats): the matrix to divide, no default
+        div (integer or float): divisor, no default
+
+    Returns:
+        New Matrix
+    Raises:
+        TypeError: if rows in matrix arent same size
+        TypeError: 
+    """
+
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    a = int(a)
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    b = int(b)
+    return (a + b)
