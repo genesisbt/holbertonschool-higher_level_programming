30695283ef3d19813499c42bdc871a195e4e9397
german <gerchureloco@gmail.com>
2024-02-02 13:50:55 -0800

    hardest excercise till now

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -9,20 +9,33 @@ def matrix_divided(matrix, div):
     Receives a matrix and a divisor.
 
     Args:
-        matrix (list of lists of integers or floats): the matrix to divide, no default
+        matrix (list of lists of integers or floats):
+        the matrix to divide, no default
         div (integer or float): divisor, no default
 
     Returns:
         New Matrix
     Raises:
         TypeError: if rows in matrix arent same size
-        TypeError: 
+        TypeError: if matrix isnt a list of list of integers amd/or floats
+        TypeError: if div isnt integer or float
+        ZeroDivisionError: if div = 0
     """
-
-    if not isinstance(a, (int, float)):
-        raise TypeError("a must be an integer")
-    a = int(a)
-    if not isinstance(b, (int, float)):
-        raise TypeError("b must be an integer")
-    b = int(b)
-    return (a + b)
+    sublength = len(matrix[0])
+    newmatrix = []
+    for sublist in matrix:
+        if not isinstance(div, (float, int)):
+            raise TypeError("div must be a number")
+        if div == 0:
+            raise ZeroDivisionError("division by zero")
+        if len(sublist) != sublength:
+            raise TypeError("Each row of the matrix must have the same size")
+        newlist = []
+        for number in sublist:
+            if not isinstance(number, (float, int)):
+                raise TypeError("matrix must be a \
+matrix (list of lists) of integers/floats")
+            numtemp = round((number / div), 2)
+            newlist.append(int(numtemp) if numtemp % 1 == 0 else numtemp)
+        newmatrix.append(newlist)
+    return newmatrix
