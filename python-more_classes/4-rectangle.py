f14ca0f395d808f5a8b67defe162fe7b45bd6be9
german <gerchureloco@gmail.com>
2024-02-05 18:44:30 -0800

    excercise 4 fix

--- python-more_classes/4-rectangle.py
+++ python-more_classes/4-rectangle.py
@@ -53,5 +53,6 @@ class Rectangle:
             return ""
         else:
             return ('\n'.join(("#" * self.width for a in range(self.height))))
+
     def __repr__(self):
         return ("Rectangle({},{})".format(self.width, self.height))
