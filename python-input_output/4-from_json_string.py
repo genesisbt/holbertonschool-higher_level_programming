9590c06ad0b240fb555dec89fcb72b11a4150949
german <gerchureloco@gmail.com>
2024-02-09 15:17:06 -0800

    input and output 0

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -1,2 +1,12 @@
 #!/usr/bin/python3
+"""
+module for from_json_string function
+"""
+importjson
+
+
 def from_json_string(my_str):
+    """
+    function returns a python data structure from a json string
+    """
+    return json.loads(my_str)
