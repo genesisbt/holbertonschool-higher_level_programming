93fd1f4f8735876c7173fe0571781d883be328a9
german <gerchureloco@gmail.com>
2024-02-09 15:28:02 -0800

    input and output 0

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -1,2 +1,13 @@
 #!/usr/bin/python3
-def read_file(filename=""):
+"""
+module for save_to_json_file function
+"""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """
+    function to save json string representation into file
+    """
+    with open(filename, 'w') as f:
+        f.write(json.dumps(my_obj))
