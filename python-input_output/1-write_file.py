f6d5f97f0a94925048f786fadecd397aab5e6f8f
german <gerchureloco@gmail.com>
2024-02-09 14:59:06 -0800

    input and output 0

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,2 +1,12 @@
 #!/usr/bin/python3
+"""
+module to define write_file function
+"""
+
+
 def write_file(filename="", text=""):
+    """
+    function to write a string to a file
+    """
+    with open(filename, 'w', encoding="utf-8") as f:
+        return f.write(text)
