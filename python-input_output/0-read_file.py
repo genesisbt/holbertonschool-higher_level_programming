d1ba43b15eac85ba52e56ec35276421910d97517
german <gerchureloco@gmail.com>
2024-02-09 14:49:54 -0800

    input and output 0

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
+"""
+Defines a function read_file
+"""
+
 
 def read_file(filename=""):
+    """
+    Function for printing a file content as String
+    """
     with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
-    f.closed
