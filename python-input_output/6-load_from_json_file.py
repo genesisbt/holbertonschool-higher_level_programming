694d458a1f65b99a14e533a1bd09de4d2688282e
german <gerchureloco@gmail.com>
2024-02-09 15:53:09 -0800

    input and output 0

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -1,2 +1,13 @@
 #!/usr/bin/python3
-def read_file(filename=""):
+"""
+module for load_from_json_file
+"""
+import json
+
+
+def load_from_json_file(filename):
+    """
+    makes a json object from the json string representation in a file
+    """
+    with open(filename) as f:
+        return json.loads(f.read())
