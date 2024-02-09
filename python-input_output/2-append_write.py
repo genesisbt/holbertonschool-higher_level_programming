54b4ae4881197674e52fbf038d32789940fbf6ea
german <gerchureloco@gmail.com>
2024-02-09 15:02:45 -0800

    input and output 0

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,2 +1,12 @@
 #!/usr/bin/python3
+"""
+Module to append_write function
+"""
+
+
 def append_write(filename="", text=""):
+    """
+    function to append a string at the eof
+    """
+    with open(filename, 'w') as f:
+        return f.write(text)
