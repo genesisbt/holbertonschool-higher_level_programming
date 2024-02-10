b9e27f1f7b753e6441aa6a966af1f5bb6077bd6e
german <gerchureloco@gmail.com>
2024-02-09 17:12:14 -0800

    input and output 0

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -5,13 +5,14 @@ module to add all arguments to a python list and save to a file
 from sys import argv
 save = __import__("5-save_to_json_file").save_to_json_file
 load = __import__("6-load_from_json_file").load_from_json_file
-filename = "add_item.json"
+
 
 try:
-    lista = load(filename)
+    lista = load("add_item.json")
 except FileNotFoundError:
     lista = []
 
-    for argnum in range(1, len(argv)):
-        lista.append(argv[argnum])
-save(lista, filename)
+for argnum in range(1, len(argv)):
+    lista.append(argv[argnum])
+
+save(lista, "add_item.json")
