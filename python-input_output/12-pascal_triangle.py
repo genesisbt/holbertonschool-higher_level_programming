86c6fa43b705ce8313ba1410ec6d8e0f0863cdab
german <gerchureloco@gmail.com>
2024-02-09 19:03:11 -0800

    12 hard but good

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -7,11 +7,14 @@ def pascal_triangle(n):
     final_list = []
     lastsublist = [0]
     for num in range(n):
-        sublist = []
-        for subnum in range(num +1):
-            if num > 0 and subnum < num :
-                sublist.append(lastsublist[subnum] + lastsublist[subnum])
+        sublist = [1]
+        for subnum in range(num):
+            if num > 0 and subnum < num - 1:
+                sublist.append(lastsublist[subnum] + lastsublist[subnum + 1])
+        if (num == 0):
+            final_list.append([1])
+        else:
+            final_list.append(sublist)
         sublist.append(1)
         lastsublist = sublist.copy()
-        final_list.append(sublist)
     return final_list
