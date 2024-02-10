7e9da6086c60aa6c3a56c3d010910e8667904547
german <gerchureloco@gmail.com>
2024-02-09 16:58:54 -0800

    input and output 0

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -2,12 +2,10 @@
 """
 module to add all arguments to a python list and save to a file
 """
-
-
 from sys import argv
-import json
 save = __import__("5-save_to_json_file").save_to_json_file
 load = __import__("6-load_from_json_file").load_from_json_file
+
 
 try:
     lista = load("add_item.json")
