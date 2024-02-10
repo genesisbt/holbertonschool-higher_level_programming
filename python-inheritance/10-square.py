74f04ab2150ba3439b372275a6b54b252a5c678b
german <gerchureloco@gmail.com>
2024-02-10 10:27:45 -0800

    10

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+module for class rectangle
+"""
+Rectangle = __import__('9-Rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    Defines Class Rectangle, subclass of BaseGeometry
+    """
+    def __init__(self, sidesize):
+        self.integer_validator("sidesize", sidesize)
+        self.__sidesize = sidesize
+
+    def __str__(self):
+        return "[Rectangle] {}/{}".format(self.__sidesize, self.__sidesize)
+
+    def area(self):
+        return self.__sidesize **
