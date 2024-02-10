7bbfcfbeb3420a59b0c8be15f65a90cbc543a0da
german <gerchureloco@gmail.com>
2024-02-09 17:21:55 -0800

    input and output 0

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -1,2 +1,17 @@
 #!/usr/bin/python3
-def read_file(filename=""):
+"""
+This module contains the class student
+"""
+
+
+class student:
+    """
+    Defines the class student, its atributes and functions
+    """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        return self.__dict__
