8d3006b618eb5599dde85e35bda23995fcf4c064
german <gerchureloco@gmail.com>
2024-02-09 18:01:10 -0800

    11

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -1,2 +1,33 @@
 #!/usr/bin/python3
-def read_file(filename=""):
+"""
+This module contains the class student
+"""
+
+
+class Student:
+    """
+    Defines the class student, its atributes and functions
+    """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        return self.__dict_
+
+    def to_json(self, attrs=None):
+
+        if isinstance(attrs, list):
+            if all(isinstance(name, str) for name in attrs):
+                add_dict = {}
+                for name in attrs:
+                    if hasattr(self, name):
+                        add_dict[name] = getattr(self, name)
+                return add_dict
+        else:
+            return self.__dict__
+
+    def reload_from_json(self, json):
+        for key in json:
+            settattr(self, key, json[key])
