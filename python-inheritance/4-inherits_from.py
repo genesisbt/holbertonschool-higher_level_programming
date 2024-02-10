52096bb978ef5178a0fd5ca4bb6911cf413c035f
german <gerchureloco@gmail.com>
2024-02-10 09:24:23 -0800

    5

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+module for inherits from function
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    checks if object is a subclass only
+    """
+    return isinstance(obj, a_class) and type(obj) is not a class
---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+module for inherits from function
+"""
+
+
+    class BaseGeometry:
+    """
+    checks if object is a subclass only
+    """
