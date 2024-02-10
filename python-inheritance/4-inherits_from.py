82b982af0334598be3ed4ab6ead8b27715270c27
german <gerchureloco@gmail.com>
2024-02-10 11:19:05 -0800

    4 fix

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -4,8 +4,8 @@ module for inherits from function
 """
 
 
-def is_kind_of_class(obj, a_class):
+def inherits_from(obj, a_class):
     """
     checks if object is a subclass only
     """
-    return isinstance(obj, a_class) and type(obj) is not a class
+    return isinstance(obj, a_class) and type(obj) is not a_class
