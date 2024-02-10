dd677f1a4cbe62998d1452746abe247617b9dbaa
german <gerchureloco@gmail.com>
2024-02-10 09:14:58 -0800

    2

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Module for function is same class
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    function to check if an object belongs to a class
+    """
+    return type(obj) is a_class
