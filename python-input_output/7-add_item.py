4a8befb6add8fa1d59d73906823714ca568cf2fe
german <gerchureloco@gmail.com>
2024-02-09 16:53:20 -0800

    input and output 0

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,2 +1,18 @@
 #!/usr/bin/python3
-def read_file(filename=""):
+"""
+module to add all arguments to a python list and save to a file
+"""
+
+from sys import argv
+import json
+save = __import__("5-save_to_json_file").save_to_json_file
+load = __import__("6-load_from_json_file").load_from_json_file
+
+try:
+    lista = load("add_item.json")
+except FileNotFoundError:
+    lista = []
+
+    for argnum in range(1, len(argv)):
+        lista.append(argv[argnum])
+save(lista, "add_item.json")
