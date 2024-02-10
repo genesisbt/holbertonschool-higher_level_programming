932b3a3278e3a34af56e69db28a48fba81f4d7a8
german <gerchureloco@gmail.com>
2024-02-09 19:15:35 -0800

    0

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Module for function lookup
+"""
+
+
+def lookup(obj):
+    """ 
+    function returns list of attributes and mehtods from an object
+    """
+    return dir(object)
