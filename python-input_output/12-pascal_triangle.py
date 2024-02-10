17a622f94c0554b56c36f3ba81e2be9ac49e6e6c
german <gerchureloco@gmail.com>
2024-02-09 18:45:35 -0800

    11

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -1,2 +1,17 @@
 #!/usr/bin/python3
-def read_file(filename=""):
+"""
+Module for pascal_triangle function
+"""
+
+def pascal_triangle(n):
+    final_list = []
+    lastsublist = [0]
+    for num in range(n):
+        sublist = []
+        for subnum in range(num +1):
+            if num > 0 and subnum < num :
+                sublist.append(lastsublist[subnum] + lastsublist[subnum])
+        sublist.append(1)
+        lastsublist = sublist.copy()
+        final_list.append(sublist)
+    return final_list
