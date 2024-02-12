f0ad7ac15083b7811ef582cbd48b0db188082652
german <gerchureloco@gmail.com>
2024-02-12 13:47:53 -0800

    json dumps validation fix try

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -22,7 +22,7 @@ class Base:
 
     def to_json_string(list_dictionaries):
         """turns a list of dict into a json string representation"""
-        if len(list_dictionaries) == 0 or list_dictionaries is None:
+        if not len(list_dictionaries) == 0 or list_dictionaries is None:
             return "[]"
         else:
             return json.dumps(list_dictionaries)
