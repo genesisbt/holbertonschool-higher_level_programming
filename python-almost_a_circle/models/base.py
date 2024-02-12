0551a57a885c8457d3ed6545d83712ddf9a9645a
german <gerchureloco@gmail.com>
2024-02-12 13:43:44 -0800

    json dumps validation

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -22,4 +22,7 @@ class Base:
 
     def to_json_string(list_dictionaries):
         """turns a list of dict into a json string representation"""
-        return json.dumps(list_dictionaries)
+        if not list_dictionaries or list_dictionaries is None:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
